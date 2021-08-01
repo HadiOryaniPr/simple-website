3fa74f2ae088acc0680b17aa6fcde53f591ff64f
hadioryani <Hadioryanipr@gmail.com>
2021-08-01 23:17:32 +0430

    add responsive style menu

---
+++ app.js
@@ -0,0 +1,6 @@
+const icon = document.querySelector('.fa-bars');
+const menu = document.querySelector('.menu');
+
+icon.addEventListener('click', function(){
+     menu.classList.toggle('showmenu')
+})
