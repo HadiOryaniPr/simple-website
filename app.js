a6a67c8dbe238d23647ef7b3da207f0935cfa3ad
hadioryani <Hadioryanipr@gmail.com>
2021-08-01 23:30:35 +0430

    add js file for hambureger menu

--- app.js
+++
@@ -1,6 +0,0 @@
-const icon = document.querySelector('.fa-bars');
-const menu = document.querySelector('.menu');
-
-icon.addEventListener('click', function(){
-     menu.classList.toggle('showmenu')
-})
