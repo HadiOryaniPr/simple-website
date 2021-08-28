fa2e25cd7974d62a13b783f7e501443c437d1c11
Hadi Oryani <83688429+HadiOryaniPr@users.noreply.github.com>
2021-08-28 22:14:28 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var icon = document.getElementById("icon");
-var menu = document.querySelector(".menu");
+let icon = document.getElementById("icon");
+ menu = document.querySelector(".menu");
 
 icon.addEventListener("click", function () {
   menu.classList.toggle("showmenu");
